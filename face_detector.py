2fd2db3c409d8b1819e9cdc2576a70619592cf77
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-26 22:30:42 -0700

    grayscale img

--- face_detector.py
+++ face_detector.py
@@ -3,4 +3,18 @@ import cv2
 # loading trained images thanks to opencv
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# load in image that is detected
+img = cv2.imread('isayama.jpg')
+
+# converted to gray scale so easier on machine
+grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# showing image, first param is name of window
+cv2.imshow('Face detector', grayscale_img)
+
+# this will wait until a key is pressed (so you can view image)
+cv2.waitKey()
+
+
+
 print("Code completed")
