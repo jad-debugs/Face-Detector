8f9ec3846e0d427f51e2dafc2eb0e0d652e1120a
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-26 22:08:42 -0700

    files messed up, fixed now

--- Face-Detector/face_detector.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-# loading trained images thanks to opencv
-trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
-print("Code completed")
--- face_detector.py
+++ face_detector.py
@@ -1,3 +1,6 @@
+import cv2
 
+# loading trained images thanks to opencv
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 print("Code completed")
